fdfb72ffd7e90cfe00c92c2b268852df8444d7b5
xeho91 <xeho91@pm.me>
2023-11-15 20:27:00 +0800

    🚧 WIP

--- editors/nvim/lazyvim/lua/plugins/rust.lua
+++ editors/nvim/lazyvim/lua/plugins/rust.lua
@@ -34,37 +34,37 @@ return {
 			{ mode = "n", "<leader>ccC", function() return require("crates").open_crates_io() end, expr = false, desc = "Crates - open crates.io" },
 		},
 	},
-	{
-		-- https://github.com/simrat39/rust-tools.nvim
-		"simrat39/rust-tools.nvim",
-		opts = function()
-			local ok, mason_registry = pcall(require, "mason-registry")
-			local adapter ---@type any
-			if ok then
-				-- rust tools configuration for debugging support
-				local codelldb = mason_registry.get_package("codelldb")
-				local extension_path = codelldb:get_install_path() .. "/extension/"
-				local codelldb_path = extension_path .. "adapter/codelldb"
-				local liblldb_path = vim.fn.has("mac") == 1 and extension_path .. "lldb/lib/liblldb.dylib"
-					or extension_path .. "lldb/lib/liblldb.so"
-				adapter = require("rust-tools.dap").get_codelldb_adapter(codelldb_path, liblldb_path)
-			end
-			return {
-				dap = {
-					adapter = adapter,
-				},
-				tools = {
-					on_initialized = function()
-						vim.cmd([[
-							augroup RustLSP
-								autocmd CursorHold                      *.rs silent! lua vim.lsp.buf.document_highlight()
-								autocmd CursorMoved,InsertEnter         *.rs silent! lua vim.lsp.buf.clear_references()
-								autocmd BufEnter,CursorHold,InsertLeave *.rs silent! lua vim.lsp.codelens.refresh()
-							augroup END
-						]])
-					end,
-				},
-			}
-		end,
-	},
+	-- {
+	-- 	-- https://github.com/simrat39/rust-tools.nvim
+	-- 	"simrat39/rust-tools.nvim",
+	-- 	opts = function()
+	-- 		local ok, mason_registry = pcall(require, "mason-registry")
+	-- 		local adapter ---@type any
+	-- 		if ok then
+	-- 			-- rust tools configuration for debugging support
+	-- 			local codelldb = mason_registry.get_package("codelldb")
+	-- 			local extension_path = codelldb:get_install_path() .. "/extension/"
+	-- 			local codelldb_path = extension_path .. "adapter/codelldb"
+	-- 			local liblldb_path = vim.fn.has("mac") == 1 and extension_path .. "lldb/lib/liblldb.dylib"
+	-- 				or extension_path .. "lldb/lib/liblldb.so"
+	-- 			adapter = require("rust-tools.dap").get_codelldb_adapter(codelldb_path, liblldb_path)
+	-- 		end
+	-- 		return {
+	-- 			dap = {
+	-- 				adapter = adapter,
+	-- 			},
+	-- 			tools = {
+	-- 				on_initialized = function()
+	-- 					vim.cmd([[
+	-- 						augroup RustLSP
+	-- 							autocmd CursorHold                      *.rs silent! lua vim.lsp.buf.document_highlight()
+	-- 							autocmd CursorMoved,InsertEnter         *.rs silent! lua vim.lsp.buf.clear_references()
+	-- 							autocmd BufEnter,CursorHold,InsertLeave *.rs silent! lua vim.lsp.codelens.refresh()
+	-- 						augroup END
+	-- 					]])
+	-- 				end,
+	-- 			},
+	-- 		}
+	-- 	end,
+	-- },
 }
