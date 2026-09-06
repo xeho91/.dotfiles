79747c6881e7d853c065acbae331b760deb3bb42
Mateusz Kadlubowski <xeho91@pm.me>
2026-09-06 11:39:29 +0200

    LazyVim(snacks): Add copy notification

--- editors/nvim/lazyvim/lua/plugins/snacks.lua
+++ editors/nvim/lazyvim/lua/plugins/snacks.lua
@@ -9,6 +9,27 @@ return {
 				explorer = {
 					auto_open = false,
 				},
+				notifications = {
+				actions = {
+					copy_notification = function(picker, item)
+						if item and item.item then
+							vim.fn.setreg("+", item.item.msg)
+							vim.notify("Copied notification content")
+						end
+					end,
+				},
+				win = {
+					input = {
+						keys = {
+							["<C-y>"] = {
+								"copy_notification",
+								mode = { "n", "i" },
+								desc = "Copy notification",
+							},
+						},
+					},
+				},
+			},
 			},
 		},
 	},
