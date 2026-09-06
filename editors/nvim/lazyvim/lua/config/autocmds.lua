645eaf5b961dca87dc4c6452e013db623a74cf7a
Mateusz Kadlubowski <xeho91@pm.me>
2026-09-06 11:40:11 +0200

    LazyVim(autocmds): Add `:RelativePath`, `:AbsolutePath`, `:Filename`

--- editors/nvim/lazyvim/lua/config/autocmds.lua
+++ editors/nvim/lazyvim/lua/config/autocmds.lua
@@ -6,3 +6,20 @@ vim.api.nvim_create_augroup("lazyvim_wrap_spell", { clear = true })
 vim.api.nvim_create_autocmd("FocusGained", {
 	command = "checktime",
 })
+
+local function current_buffer_path(modifier)
+	local path = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(0), modifier)
+	print(path ~= "" and path or "[No file name]")
+end
+
+vim.api.nvim_create_user_command("RelativePath", function()
+	current_buffer_path(":.")
+end, { desc = "Print the current buffer's relative path" })
+
+vim.api.nvim_create_user_command("AbsolutePath", function()
+	current_buffer_path(":p")
+end, { desc = "Print the current buffer's absolute path" })
+
+vim.api.nvim_create_user_command("Filename", function()
+	current_buffer_path(":t")
+end, { desc = "Print the current buffer's filename" })
